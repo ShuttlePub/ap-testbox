c9c98ea81650aa5bf511de202398b5bbd002bfb7
ReiRokusanami0010 <reirokusanami.rdh@gmail.com>
2025-03-28 21:31:18 +0900

    Added to generate certificates for `ShuttlePub`.

--- gencert/mkcert.sh
+++ gencert/mkcert.sh
@@ -2,3 +2,4 @@
 
 cp /root/.local/share/mkcert/rootCA.pem /certs/root/
 mkcert -cert-file /certs/nginx/misskey.crt -key-file /certs/nginx/misskey.key misskey.localhost
+mkcert -cert-file /certs/nginx/shuttlepub.crt -key-file /certs/nginx/shuttlepub.key shuttlepub.localhost
